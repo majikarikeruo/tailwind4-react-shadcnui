3ca44282c45842ae8c9f69b9a0d16986dad5582f
majikarikeruo <castero1219@gmail.com>
2025-02-22 21:47:23 +0900

    [add]first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <Button className="bg-red-500">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
